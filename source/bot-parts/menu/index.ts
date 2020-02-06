242d4a43a5921fe4fa423b9ed2af06d9a742982d
EdJoPaTo <edjopato@gmail.com>
2020-02-06 09:48:20 +0100

    feat: show public key in main menu

--- source/bot-parts/menu/index.ts
+++ source/bot-parts/menu/index.ts
@@ -1,12 +1,39 @@
 import {Composer} from 'telegraf'
 import TelegrafInlineMenu from 'telegraf-inline-menu'
+import {markdown as format} from 'telegram-format'
+
+import {Session} from '../../types'
 
 import {menu as myTablesMenu} from './my-tables'
 import {menu as openTablesMenu} from './open-tables'
 
-const mainMenu = new TelegrafInlineMenu(ctx => {
-	return `Hi ${ctx.from!.first_name}!`
-})
+function menuText(ctx: any): string {
+	const session = ctx.session as Session
+
+	let text = ''
+
+	text += 'Hi '
+	text += ctx.from.first_name
+	text += '!\n\n'
+
+	text += 'My public key is:'
+	text += ' '
+	text +=	format.monospace(shortPubKey(session.pubKey))
+	text += '\n'
+	text +=	format.monospace(session.pubKey)
+
+	return text
+}
+
+function shortPubKey(pubKey: string): string {
+	let short = ''
+	short += pubKey.slice(0, 4)
+	short += '-'
+	short += pubKey.slice(-4)
+	return short
+}
+
+const mainMenu = new TelegrafInlineMenu(menuText)
 mainMenu.setCommand('start')
 
 mainMenu.submenu('My tables', 'my', myTablesMenu)
